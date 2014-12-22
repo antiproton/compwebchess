feade49560e6a9e3c026f159fe1f8019d7cc032f
antiproton <dr.urban@netreal.de>
2014-12-22 20:35:06 +0100

    footer

---
+++ footer.php
@@ -0,0 +1,9 @@
+<?php
+$timestamp = time();
+
+$datum = date("d.m.Y",$timestamp);
+$uhrzeit = date("H:i",$timestamp);
+echo $datum," - ",$uhrzeit," Uhr - ";
+
+?>
+Combwebchess 2015 - by Dr. R. Urban
