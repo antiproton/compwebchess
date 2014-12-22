c775830606eb3058d367f2e1bc6027f545cce8b7
antiproton <dr.urban@netreal.de>
2014-12-22 20:35:22 +0100

    config

---
+++ includes/config.php
@@ -0,0 +1,26 @@
+<?php
+#------------------------------------------------------------------------------------------#
+#  Combwebchess                                                                            #
+#  Copyright (c) Dr. R. Urban                                                              #
+#  22.12.2014                                                                              #
+#  Released under the MIT license                                                          #
+#  https://github.com/antiproton                                                           #
+#------------------------------------------------------------------------------------------#
+error_reporting(E_ALL);
+$debug = true;
+
+
+/* database settings */
+$CFG_SERVER = "localhost";
+$CFG_USER = "root";
+$CFG_PASSWORD = "";
+$CFG_DATABASE = "webchess";
+
+$CFG_MAILADDRESS = "dr.urban@netreal.de";   /* email address people see when receiving WebChess generated mail */
+$CFG_SITE_URL = "http://localhost/GitHub/compwebchess";    /* Enter the url to your webchess installation ...no backslash!*/
+$CFG_SITE_ROOT = "C:/xampp/htdocs/GitHub/compwebchess";     /* Enter the absolute path to your webchess installation...no backslash! */
+$CFG_LOG_PATH = "C:/xampp/htdocs/GitHub/compwebchess";      /* Enter the absolute path to your webchess.log file...no backslash! */
+$CFG_SITE_NAME = "Combwebchess"; 
+
+
+?>
